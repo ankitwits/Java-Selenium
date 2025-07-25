5b4d55d4a9ec08a3e7ce6aa5c8daba2817087851
Ankit Shrivastava <ankitwits@gmail.com>
2025-07-25 15:28:31 +0530

    Docker file and folder added

---
+++ src/Docker/JavafilesforImage/SquareValue.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class SquareValue {    
+    public static void main(String[] args) {
+        int num = 4;
+        System.out.println("Square of " + num + " is " + (num * num));
+    
+
+
+    }
+}
